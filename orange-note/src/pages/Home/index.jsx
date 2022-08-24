647bbd6751135fda36bf5e614ab501982dbcd1ad
Guilherme <guivaz1993@gmail.com>
2022-08-23 23:59:24 -0300

    Layout home done

--- orange-note/src/pages/Home/index.jsx
+++ orange-note/src/pages/Home/index.jsx
@@ -1,15 +1,70 @@
 import Header from "../../components/Header";
+import ProgressBar from "../../components/ProgressBar";
 import "./style.css";
 
 function Home() {
   return (
     <>
       <Header />
-      <div style={{ marginTop: "6rem" }}>
+      <section className="HomePage">
         <h1>
           Dashboard
         </h1>
-      </div>
+        <section className="MainInfos">
+          <article className="Card">
+            <div className="HeaderCard">
+              Card 1
+            </div>
+            <div className="BodyCard">
+
+              Card 1 card 1 Card 1 card 1 Card 1 card 1 Card 1 card 1
+              Card 1 card 1 Card 1 card 1 Card 1 card 1
+            </div>
+          </article>
+          <article className="Card">
+            <div className="HeaderCard">
+              Card 2
+            </div>
+            <div className="BodyCard">
+              Card 1 card 1 Card 1 card 1 Card 1 card 1 Card 1 card 1
+              Card 1 card 1 Card 1 card 1 Card 1 card 1
+            </div>
+          </article>
+          <article className="Card">
+            <div className="HeaderCard">
+              Card 3
+            </div>
+            <div className="BodyCard">
+              <p>
+                Card 1 card 1 Card 1 card 1 Card 1 card 1 Card 1 card 1
+                Card 1 card 1 Card 1 card 1 Card 1 card 1
+              </p>
+              <ProgressBar progress={50} />
+            </div>
+          </article>
+        </section>
+        <section className="GraphInfos">
+          <article className="GraphCard">
+            <div className="HeaderCard">
+              Card 1
+            </div>
+            <div className="BodyCard">
+
+              Card 1 card 1 Card 1 card 1 Card 1 card 1 Card 1 card 1
+              Card 1 card 1 Card 1 card 1 Card 1 card 1
+            </div>
+          </article>
+          <article className="GraphCard">
+            <div className="HeaderCard">
+              Card 2
+            </div>
+            <div className="BodyCard">
+              Card 1 card 1 Card 1 card 1 Card 1 card 1 Card 1 card 1
+              Card 1 card 1 Card 1 card 1 Card 1 card 1
+            </div>
+          </article>
+        </section>
+      </section>
     </>
   );
 }
