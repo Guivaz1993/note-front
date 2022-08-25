3db6f9ad377ec7de667647a96c25e5bf86db65f8
Guilherme <guivaz1993@gmail.com>
2022-08-24 21:26:37 -0300

    finish table and text studies

--- orange-note/src/components/Tables/Studies/index.jsx
+++ orange-note/src/components/Tables/Studies/index.jsx
@@ -6,52 +6,57 @@ import TableCell, { tableCellClasses } from "@mui/material/TableCell";
 import TableContainer from "@mui/material/TableContainer";
 import TableHead from "@mui/material/TableHead";
 import TableRow from "@mui/material/TableRow";
-import Paper from "@mui/material/Paper";
 
-const StyledTableCell = styled(TableCell)(({ theme }) => ({
+const StyledTableCell = styled(TableCell)(() => ({
+  "&": {
+    fontFamily: "Metropolis,sans-serif",
+    cursor: "pointer",
+    // textAlign: "center",
+  },
   [`&.${tableCellClasses.head}`]: {
-    backgroundColor: theme.palette.common.black,
-    color: theme.palette.common.white,
+    border: "0.05rem solid var(--colour-light-grey)",
+    backgroundColor: "var(--colour-grey)",
+    color: "var(--colour-white)",
+    fontSize: "2rem",
+    fontWeight: "bold",
   },
   [`&.${tableCellClasses.body}`]: {
-    fontSize: 14,
+    fontSize: "1.6rem",
+    color: "var(--colour-grey)",
+    border: "none",
+  },
+  [`&.${tableCellClasses.body}:first-of-type `]: {
+    fontWeight: "bold",
+    borderRadius: "12px 0 0 12px",
+  },
+  [` &.${tableCellClasses.body}:last-child `]: {
+    borderRadius: "0 12px 12px 0 ",
+  },
+  [`&.${tableCellClasses.body}:hover`]: {
   },
 }));
 
-const StyledTableRow = styled(TableRow)(({ theme }) => ({
-  "&": {
-    cursor: "pointer",
-  },
+const StyledTableRow = styled(TableRow)(() => ({
   "&:nth-of-type(odd)": {
-    backgroundColor: theme.palette.action.hover,
+    backgroundColor: "var(--colour-light-grey)",
+  },
+  "&:nth-of-type(odd):hover , &:nth-of-type(even):hover": {
+    textDecoration: "underline",
+    backgroundColor: "var(--colour-orange)",
+    // border: "1px solid var(--colour-grey)",
+  },
+  "&:nth-of-type(even)": {
+    backgroundColor: "#9f9b93",
   },
   // hide last border
   "&:last-child td, &:last-child th": {
     border: 0,
   },
-  "&:hover": {
-    opacity: 0.8,
-  },
 }));
 
-function createData(id, area, topic, total, done) {
-  return {
-    id, area, topic, total, done,
-  };
-}
-
-const rowsDefault = [
-  createData(1, "Front End", "JavaScript", 10, 8),
-  createData(2, "Front End", "React", 5, 4),
-  createData(3, "Back end", "JavaScript", 6, 1),
-  createData(4, "UX/UI", "UX Research", 8, 2),
-  createData(5, "Mobile", "React Native", 2, 2),
-];
-
-export default function StudiesTable() {
+export default function StudiesTable({ rows, setRows }) {
   const [order, setOrder] = useState("asc");
   const [orderBy, setOrderBy] = useState("area");
-  const [rows, setRows] = useState(rowsDefault);
 
   function handleSort(colunm) {
     const localRows = rows;
@@ -65,11 +70,10 @@ export default function StudiesTable() {
       setRows(localRows);
     }
     setOrderBy(colunm);
-    console.log(colunm, rows, order);
   }
 
   return (
-    <TableContainer component={Paper}>
+    <TableContainer sx={{ background: "var(--colour-black)" }}>
       <Table sx={{ minWidth: 700 }} aria-label="customized table">
         <TableHead>
           <TableRow>
@@ -80,21 +84,21 @@ export default function StudiesTable() {
               Área de estudo
             </StyledTableCell>
             <StyledTableCell
-              align="right"
               sortDirection={orderBy === "topic" ? order : false}
               onClick={() => handleSort("topic")}
+              align="center"
             >
               Tópicos
             </StyledTableCell>
             <StyledTableCell
-              align="right"
+              align="center"
               sortDirection={orderBy === "total" ? order : false}
               onClick={() => handleSort("total")}
             >
-              Total
+              Conteúdos
             </StyledTableCell>
             <StyledTableCell
-              align="right"
+              align="center"
               sortDirection={orderBy === "done" ? order : false}
               onClick={() => handleSort("done")}
             >
@@ -104,13 +108,32 @@ export default function StudiesTable() {
         </TableHead>
         <TableBody>
           {rows.map((row) => (
-            <StyledTableRow key={row.id} onClick={() => console.log(`Clicou no ${row.area} - ${row.topic}`)}>
-              <StyledTableCell component="th" scope="row">
+            <StyledTableRow
+              key={row.id}
+              onClick={() => console.log(`Clicou no ${row.area} - ${row.topic}`)}
+              hover
+            >
+              <StyledTableCell>
                 {row.area}
               </StyledTableCell>
-              <StyledTableCell align="right">{row.topic}</StyledTableCell>
-              <StyledTableCell align="right">{row.total}</StyledTableCell>
-              <StyledTableCell align="right">{row.done}</StyledTableCell>
+              <StyledTableCell
+                align="center"
+              >
+                {row.topic}
+
+              </StyledTableCell>
+              <StyledTableCell
+                align="center"
+              >
+                {row.total}
+
+              </StyledTableCell>
+              <StyledTableCell
+                align="center"
+              >
+                {row.done}
+
+              </StyledTableCell>
             </StyledTableRow>
           ))}
         </TableBody>
--- orange-note/src/pages/Studies/index.jsx
+++ orange-note/src/pages/Studies/index.jsx
@@ -1,16 +1,40 @@
+/* eslint-disable max-len */
+import { useState } from "react";
 import Header from "../../components/Header";
 import StudiesTable from "../../components/Tables/Studies";
 import "./style.css";
 
+function createData(id, area, topic, total, done) {
+  return {
+    id, area, topic, total, done,
+  };
+}
+
+const rowsDefault = [
+  createData(1, "Front End", "JavaScript", 10, 8),
+  createData(2, "Front End", "React", 5, 4),
+  createData(3, "Back end", "JavaScript", 6, 1),
+  createData(4, "UX/UI", "UX Research", 8, 2),
+  createData(5, "Mobile", "React Native", 2, 2),
+];
+
 function Studies() {
+  const [rows, setRows] = useState(rowsDefault);
   return (
     <>
       <Header />
       <div className="StudiesPage">
-        <h1>
-          Organizador
+        <h1 className="StudiesTitle">
+          Meus estudos
         </h1>
-        <StudiesTable />
+        <p className="StudiesText">
+          Acompanhe, adicione, conclua seus estudos.
+          Utilize nossa plataforma e veja seus temas de estudos agrupando as aulas, vídeos, textos, livros e tudo mais que você tenha utilizado neles e ache relevante ter um acesso rápido.
+        </p>
+        <StudiesTable
+          rows={rows}
+          setRows={setRows}
+        />
       </div>
     </>
   );
